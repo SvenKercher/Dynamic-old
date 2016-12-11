714112221092d9eb034508484213e31092c55344
Ahmad Kazi <empinelx@gmail.com>
2016-12-11 10:42:24 -0800

    [Qt][Trivial][C++11] Remove the usage of Q_FOREACH

--- src/qt/transactiondesc.cpp
+++ src/qt/transactiondesc.cpp
@@ -264,14 +264,14 @@ QString TransactionDesc::toHTML(CWallet *wallet, CWalletTx &wtx, TransactionReco
     strHTML += "<b>" + tr("Transaction ID") + ":</b> " + TransactionRecord::formatSubTxId(wtx.GetHash(), rec->idx) + "<br>";
 
     // Message from normal darksilk:URI (darksilk:XyZ...?message=example)
-    Q_FOREACH (const std::pair<std::string, std::string>& r, wtx.vOrderForm)
+    for (const std::pair<std::string, std::string>& r : wtx.vOrderForm)
         if (r.first == "Message")
             strHTML += "<br><b>" + tr("Message") + ":</b><br>" + GUIUtil::HtmlEscape(r.second, true) + "<br>";
 
     //
     // PaymentRequest info:
     //
-    Q_FOREACH (const std::pair<std::string, std::string>& r, wtx.vOrderForm)
+    for (const std::pair<std::string, std::string>& r : wtx.vOrderForm)
     {
         if (r.first == "PaymentRequest")
         {
