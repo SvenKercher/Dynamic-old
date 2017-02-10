359a04fa9a799038caa1a55874984d19cf93de67
coin-info-net <coin-info.net@t-online.de>
2017-02-10 21:40:53 +0100

    Update dslkdns.cpp

--- src/dns/dslkdns.cpp
+++ src/dns/dslkdns.cpp
@@ -111,7 +111,7 @@ DslkDns::DslkDns(const char *bind_ip, uint16_t port_no,
     if(!inet_pton(AF_INET, bind_ip, &m_address.sin_addr.s_addr)) 
       m_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if(bind(m_sockfd, (struct sockaddr *) &m_address,
+    if(::bind(m_sockfd, (struct sockaddr *) &m_address,
                      sizeof (struct sockaddr_in)) < local_len2) {
       char buf[80];
       sprintf(buf, "DslkDns::DslkDns: Cannot bind to port %u", port_no);
